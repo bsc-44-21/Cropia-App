8a2cb89b2789cb730f7ce40d39f4d2cdf272e6c5
FaraiWona <Bsc-04-21@unima.ac.mw>
2026-03-10 23:36:54 -0700

    provider class with change notifier

--- lib/guards/auth_provider.dart
+++ lib/guards/auth_provider.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../services/auth_service.dart';
+
+class AuthProvider with ChangeNotifier {
+  final AuthService _authService = AuthService();
+  User? _user;
+  bool _isLoading = true;
+
+  User? get user => _user;
+  bool get isLoading => _isLoading;
+  bool get isAuthenticated => _user != null;
+
+  AuthProvider() {
+    _init();
+  }
