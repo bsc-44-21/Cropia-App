6d7a9c152de9395865894d290ccc7a20560114ae
FaraiWona <Bsc-04-21@unima.ac.mw>
2026-03-10 23:32:37 -0700

    auth class containing sign up

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,2 +1,18 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'supabase.dart';
+
+class AuthService {
+  final SupabaseClient _client = SupabaseService.client;
+
+  Future<AuthResponse> signUp({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    final response = await _client.auth.signUp(
+      email: email,
+      password: password,
+      data: {'full_name': name},
+    );
+    return response;
+  }
